e65e75db3fdae8b0575307aa02a29d8a04cfe253
njxz <asd344417512@qq.com>
2021-08-15 18:13:26 +0800

    添加week4作业

---
+++ week3/mydemo/cmd/week3/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"geektime/internal/conf"
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/file"
+	"os"
+)
+
+func decoder(kc *config.KeyValue, v map[string]interface{}) error {
+	err := json.Unmarshal(kc.Value, &v)
+	fmt.Println(err)
+	return nil
+}
+func main() {
+	pwd, _ := os.Getwd()
+	fmt.Println(pwd)
+	c := config.New(
+		config.WithSource(
+			file.NewSource("configs/week3_config.yaml"),
+		),
+	)
+	if err := c.Load(); err != nil {
+		panic(err)
+	}
+	fmt.Println(c.Value("server"))
+	var bc conf.MydemoConfig
+	if err := c.Scan(&bc); err != nil {
+		panic(err)
+	}
+	fmt.Println(bc)
+
+}
--- week3/mydemo/go.mod
+++ week3/mydemo/go.mod
@@ -3,9 +3,13 @@ module geektime
 go 1.16
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-kratos/kratos/v2 v2.0.3
 	github.com/google/wire v0.5.0
+	github.com/kr/pretty v0.1.0 // indirect
 	google.golang.org/genproto v0.0.0-20210805201207-89edb61ffb67
 	google.golang.org/grpc v1.39.1
 	google.golang.org/protobuf v1.27.1
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
